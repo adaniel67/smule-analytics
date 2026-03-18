cfd87c55451e095a66d200072e2f6689ce6b5b4b
adaniel67 <59068198+adaniel67@users.noreply.github.com>
2026-03-18 12:33:03 -0400

    Add smule_analytics/__init__.py

---
+++ smule_analytics/__init__.py
@@ -0,0 +1,3 @@
+# This file marks the `smule_analytics` folder as a Python "package".
+# A package is just a folder with an __init__.py file, which lets you
+# import code from it using: from smule_analytics import something
